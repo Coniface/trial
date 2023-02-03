2bd9ed7819d353b734bfa99b0633ee2279cacd0a
Victor Simart <simart.victor@gmail.com>
2023-02-03 14:59:53 +0200

    ✅: add getTrial unit-tests

---
+++ src/getTrial.spec.ts
@@ -0,0 +1,31 @@
+import { getTrial } from './getTrial';
+
+describe('getTrial', () => {
+
+  it('should create a promise', () => {
+    const [promise] = getTrial<void>();
+
+    expect(promise instanceof Promise).toBe(true);
+  });
+
+  it('should create a resolver', async () => {
+    const [promise, resolve] = getTrial<number>();
+    const randomNumber = 555;
+
+    resolve(randomNumber);
+
+    expect(typeof resolve).toBe('function');
+    await expect(promise).resolves.toBe(randomNumber);
+  });
+
+  it('should create a rejecter', async () => {
+    const [promise, _, reject] = getTrial<number>();
+    const error = new Error('Test error');
+
+    reject(error);
+
+    expect(typeof reject).toBe('function');
+    await expect(promise).rejects.toThrowError(error);
+  });
+
+});
