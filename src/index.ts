4c86bfdf71199b8b7511797dea5f2dff5af77585
Victor Simart <simart.victor@gmail.com>
2023-02-04 14:27:29 +0200

    🔧: add index to export tools

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export * from './getTrial';
+export * from './PromiseTimeoutError';
+export * from './trial';
+export {
+  Trial,
+  ResolvedTrial,
+  RejectedTrial,
+  TrialResolver,
+  TrialRejecter,
+  TrialTuple,
+} from './types';
